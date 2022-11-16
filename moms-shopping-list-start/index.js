990ece01c6408291844acd78f62d628fe4bfcfe8
Adeeb Ogaili <adeebnasser@gmail.com>
2022-11-16 15:15:24 -0600

    Updated Mom's shoping list code

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -1,48 +1,57 @@
-const addButton = document.querySelector(".add-button"); 
+const addButton = document.querySelector(".add-button");
 var inputText = document.querySelector(".input");
 const container = document.querySelector(".container");
- 
-
-addButton.addEventListener('click', function () { 
-    let newItem = inputText.value;
-    inputText.value = "";
-
-    let itemList = document.createElement('div');
-    let itemName = document.createElement('input');
-    let editButton = document.createElement('button');
-    let removeButton = document.createElement('button');
 
 
-    itemList.appendChild(itemName);
-    itemName.setAttribute('value', newItem);
-    itemName.classList.add('item-input');
 
-    itemList.appendChild(editButton);
-    editButton.textContent = 'EDIT';
-    editButton.classList.add('edit-button');
+function addNewItem() {
+    if (inputText.value === '') {
+        return null
+    } else {
+        let newItem = inputText.value;
+        inputText.value = "";
 
-    itemList.appendChild(removeButton);
-    removeButton.textContent = 'REMOVE';
-    removeButton.classList.add('remove-button')
-    
-
-    container.appendChild(itemList);
-    itemList.classList.add('item');
+        let itemList = document.createElement('div');
+        let itemName = document.createElement('input');
+        let editButton = document.createElement('button');
+        let removeButton = document.createElement('button');
 
 
-    removeButton.addEventListener('click', function (){
-        container.removeChild(itemList)
-    }) 
+        itemList.appendChild(itemName);
+        itemName.setAttribute('value', newItem);
+        itemName.classList.add('item-input');
 
-    editButton.addEventListener('click', function() {
-        itemName.contentEditable = true;
-        itemName.style.backgroundColor = "#dddbdb";
-      } );
+        itemList.appendChild(editButton);
+        editButton.textContent = 'EDIT';
+        editButton.classList.add('edit-button');
+
+        itemList.appendChild(removeButton);
+        removeButton.textContent = 'REMOVE';
+        removeButton.classList.add('remove-button')
+
+
+        container.appendChild(itemList);
+        itemList.classList.add('item');
+
+
+        removeButton.addEventListener('click', function () {
+            container.removeChild(itemList)
+        })
+
+        editButton.addEventListener('click', function () {
+            itemName.contentEditable = true;
+            itemName.style.backgroundColor = "#dddbdb";
+        });
+    }
+}
+
+addButton.addEventListener('click', () => {
+    addNewItem();
 })
 
-
-
-
-
-
+inputText.addEventListener('keyup', (event) => {
+    if (event.which === 13) {
+        addNewItem();
+    }
+})
 
