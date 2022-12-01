17c7a363d5c6b081c0357a178f0268f59bdf34aa
Adeeb Ogaili <adeebnasser@gmail.com>
2022-12-01 12:11:49 -0600

    updated employee record

--- employee.records/index.js
+++ employee.records/index.js
@@ -7,20 +7,21 @@ this.name = name;
 this.jobTitle = jobTitle;
 this.salary = salary;
 this.status = "Full Time"
-this.printEmployeeForm = function (){
-    employees.push(this)
 }
 
-}
-
-
-let employee1 = new Employee("Sam" , "Linguist", "$110,000.00");
+let employee1 = new Employee("Sam", "Linguist", "$110,000.00");
 employee1.status = "Contract"
 
-let employee2 = new Employee("Raph" , "Sales Person", "$65,000.00");
+let employee2 = new Employee("Raph", "Sales Person", "$65,000.00");
 employee2.status = "Part Time"
 
-let employee3 = new Employee("Zac" , "Teacher", "$55,000.00");
+let employee3 = new Employee("Zac", "Teacher", "$55,000.00");
 
+ function printEmployeeForm(){
+console.log(employee1, employee2, employee3)
+}
+
+employees.push(employee1, employee2, employee3);
+console.log(employees)
 
 
