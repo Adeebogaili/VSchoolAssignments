72999175f7fb58238ff37fa25e161f5e727bd370
Adeeb Ogaili <adeebnasser@gmail.com>
2022-12-24 20:33:09 -0600

    Axios project

--- level 2/axios-todo-project/app.js
+++ level 2/axios-todo-project/app.js
@@ -1,88 +1,204 @@
-const container = document.querySelector(".container");
-const todoForm = document.todoform
+const container = document.querySelector(".container")
+const popUpForm = document.popUpForm
+const titleInput = document.querySelector(".titleInput")
 
+// GET THE TODO'S DATA FROM THE DATABASE
 
-todoForm.addEventListener("submit", function(event){
-    event.preventDefault()
+function getData() {
+    axios
+        .get("https://api.vschool.io/adeeb/todo/")
+        .then(res => listData(res.data))
+        .catch(err => console.error(err))
+}
 
-    const newPost = {
-        title: todoForm.title.value,
-        description: todoForm.description.value,
-        imgUrl: todoForm.imgUrl.value
-    }
+// LISTS THE TODO TITLES TO THE DOM
 
-// POST REQUEST
-    axios.post("https://api.vschool.io/adeeb/todo", newPost) 
-        .then(res => console.log(res.data))
-        .catch(err => console.log(err))
-}) 
+function listData(data) {
+    clearList()
 
-// GET ALL
+    for (let i = 0; i < data.length; i++) {
 
-axios.get("https://api.vschool.io/adeeb/todo/")
-.then(response => {
-
-    for(let i = 0; i < response.data.length; i++){
-
-        // CREATE HTML 
+        // CREATE ELEMENTS
         const div = document.createElement('div')
-        const todoTitle = document.createElement('input')
-        const todoDescription = document.createElement('input')
+        const itemTitle = document.createElement('input')
+        const itemDescription = document.createElement('input')
+        const itemPrice = document.createElement('input')
+        const imageUrl = document.createElement('img')
         const checkBox = document.createElement('input')
         const editButton = document.createElement('button');
         const removeButton = document.createElement('button')
+        const saveButton = document.createElement('button')
 
-        todoTitle.value = response.data[i].title
-        todoTitle.setAttribute('readonly', true)
-        todoTitle.classList.add('title')
+        itemTitle.value = data[i].title
+        itemTitle.classList.add('title')
 
-        todoDescription.value = response.data[i].description
-        todoDescription.setAttribute('readonly', true)
-        todoDescription.classList.add('description')
+        itemDescription.value = data[i].description
+        itemDescription.classList.add('description')
 
-        removeButton.value = response.data[i]._id
+        itemPrice.value = data[i].price
+        itemPrice.classList.add('itemPrice')
+        itemPrice.setAttribute('type', 'number')
+
+        imageUrl.setAttribute('readonly', true)
+        imageUrl.classList.add('imageUrl')
+        imageUrl.src = data[i].imgUrl
+
+        removeButton.value = data[i]._id
         removeButton.textContent = "DELETE"
         removeButton.classList.add('removeBtn')
 
         checkBox.setAttribute('type', 'checkbox')
         checkBox.classList.add('checkbox')
+        checkBox.value = data[i].completed
 
         editButton.textContent = "EDIT"
         editButton.classList.add('editBtn')
+        editButton.value = data[i]._id
+
+        saveButton.textContent = "SAVE"
+        saveButton.classList.add('saveBtn')
+        saveButton.value = data[i]._id
 
         container.appendChild(div)
         div.classList.add('item')
 
         // APPEND ALL
-        div.appendChild(todoTitle)
-        div.appendChild(todoDescription)
         div.appendChild(checkBox)
+        div.appendChild(imageUrl)
+        div.appendChild(itemTitle)
+        div.appendChild(itemDescription)
+        div.appendChild(itemPrice)
         div.appendChild(editButton)
-        div.appendChild(removeButton) 
+        div.appendChild(removeButton)
+        popUpForm.appendChild(saveButton)
 
-    
+        // DELETE ITEM
         removeButton.addEventListener('click', () => {
             axios
-                .delete(`https://api.vschool.io/adeeb/todo/${response.data[i]._id}`)
+                .delete(`https://api.vschool.io/adeeb/todo/${data[i]._id}`)
                 .then(res => console.log(res))
                 .catch(err => (console.log(err)))
-                container.removeChild(div)
+            container.removeChild(div)
 
-        } )
+        })
+
+        itemTitle.disabled = true
+        itemDescription.disabled = true
+
+        // EDIT ITEM
+
+        const closeButton = document.querySelector(".closeBtn")
+        const overlay = document.getElementById("overlay")
+        const modal = document.getElementById("modal")
 
         editButton.addEventListener('click', () => {
+            console.log(data[i]._id)
+            openModal(modal)
+        })
 
-        const edited = {
-        title: "changed",
-        description: par.value,
-        imgUrl: todoForm.imgUrl.value
+        closeButton.addEventListener('click', () => {
+            closeModal(modal)
+        })
+
+        function openModal(modal) {
+            if (modal == null) return
+            modal.classList.add('active')
+            overlay.classList.add('active')
         }
-         axios
-            .put(`https://api.vschool.io/adeeb/todo/${response.data[i]._id}`, edited)
-            .then(res => console.log(res))
-            .catch(err => (console.log(err)))
 
-} )
+        function closeModal(modal) {
+            if (modal == null) return
+            modal.classList.remove('active')
+            overlay.classList.remove('active')
+        }
+
+        // UPDATE DATABASE WITH SAVE
+
+        saveButton.addEventListener('click', (event) => {
+            event.preventDefault()
+
+
+            const updated = {
+                title: popUpForm.title.value,
+                description: popUpForm.description.value,
+                imgUrl: popUpForm.imgUrl.value,
+                price: popUpForm.itemPrice.value,
+                completed: false
+            }
+            console.log(data[i]._id)
+
+            axios
+                .put(`https://api.vschool.io/adeeb/todo/${data[i]._id}`, updated)
+                .then(res => getData())
+                .catch(err => console.error(err))
+        })
+
+        // CHECKBOX CHECK
+        // if (checkBox.checked) {
+        //     itemTitle.style.textDecoration = "line-through"
+        //     itemDescription.style.textDecoration = "line-through"
+
+        //     const checked = {
+        //     completed: true,
+        //     title: itemTitle.value,
+        //     description: itemDescription.value,
+        //     imageUrl: imageUrl.value,
+        //     price: itemPrice.value,
+        //     }
+        //     axios
+        //         .put(`https://api.vschool.io/adeeb/todo/${data[i]._id}`, checked)
+        //         .catch(err => console.error(err))
+        // } else {
+
+        //     itemTitle.style.textDecoration = "none"
+        //     itemDescription.style.textDecoration = "none"
+
+        //     const notChecked = {
+        //     completed: false,
+        //     title: itemTitle.value,
+        //     description: itemDescription.value,
+        //     imageUrl: imageUrl.value,
+        //     price: itemPrice.value,
+        //     }
+        //     axios
+        //         .put(`https://api.vschool.io/adeeb/todo/${data[i]._id}`, notChecked)
+        //         .catch(err => console.error(err))
+        // }
+
     }
+}
+
+// CLEAR LIST
+function clearList() {
+    const el = document.querySelector('.container')
+    while (el.firstChild) {
+        el.removeChild(el.firstChild)
+    }
+}
+
+getData()
+
+// FORM FOR THE POST REQUEST
+
+const todoForm = document.todoform
+
+todoForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const newTodo = {
+        title: todoForm.title.value,
+        description: todoForm.description.value,
+        imgUrl: todoForm.imgUrl.value,
+        price: todoForm.itemPrice.value,
+        completed: false
+    }
+
+    todoForm.title.value = ''
+    todoForm.description.value = ''
+    todoForm.imgUrl.value = ''
+
+    axios
+        .post('https://api.vschool.io/adeeb/todo', newTodo)
+        .then(res => getData())
+        .catch(err => console.error(err))
 })
-.catch(error => console.log(error))
