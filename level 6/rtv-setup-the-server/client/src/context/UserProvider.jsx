1c1a70cfca86f9739bbf72329ea6101d6b116547
Adeeb Ogaili <adeebnasser@gmail.com>
2023-03-14 15:32:00 -0500

    added auth

---
+++ level 6/rtv-setup-the-server/client/src/context/UserProvider.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+export const UserContext = React.createContext()
+
+export default function(props) {
+
+    const initState = {
+        user: {},
+        token: "",
+        todos: []
+    }
+
+    const [userState, setUserState] = useState(initState)
+
+    function signup(credentials){
+        axios.post("/auth/signup", credentials)
+        .then(res => console.dir(res))
+        .catch(err => console.error(err.response.data.errMsg))
+    }
+
+    function login(credentials){
+        axios.post("/auth/login", credentials)
+        .then(res => console.dir(res))
+        .catch(err => console.error(err.response.data.errMsg))
+    }
+
+    return (
+        <UserContext.Provider
+            value={{
+                ...userState,
+                signup,
+                login
+            }}
+        >
+            {props.children}
+        </UserContext.Provider>
+    )
+}
+
