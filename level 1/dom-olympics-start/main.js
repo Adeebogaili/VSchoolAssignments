0581e97b7bd9a8d23afc8281603c05d01d6ffb3d
Adeeb Ogaili <adeebnasser@gmail.com>
2023-04-21 21:01:05 -0500

    fix dom min

--- level 1/dom-olympics-start/main.js
+++ level 1/dom-olympics-start/main.js
@@ -1,45 +1,78 @@
 // create an h1 and give it a class and some text and put it in the DOM.
-const h1 = document.createElement("h1");
-h1.textContent = "Javascript made this!!";
-document.getElementById("header").appendChild(h1);
-h1.className = "header";
+const h1 = document.createElement('h1');
+h1.textContent = 'Javascript made this!!';
+document.getElementById('header').appendChild(h1);
+h1.className = 'header';
 
 //  create a subtitle element and give it a class and some text and put it in the DOM.
-const subTitle = document.createElement("h3");
+const subTitle = document.createElement('h3');
+subTitle.className = 'sub-title';
 subTitle.innerHTML += `<span class="name">Adeeb</span> wrote the JavsScript`;
-subTitle.style.textAlign = "center";
-document.getElementById("header").appendChild(subTitle);
-subTitle.className = "sub-title";
+document.getElementById('header').appendChild(subTitle);
 
-/* select all divs with class message and assign a varible to it
-and change all the messages using .textContent to something pleasant.*/
+// chat
+const messagesContainer = document.getElementById('messages-container');
+const messageForm = document.forms['message-form'];
+const messageInput = document.querySelector('input[name="message-input"]');
+const submitButton = document.querySelector('button[type="submit"]');
 
-const messages = document.querySelector(".messages");
-const messageText = messages.querySelectorAll("div.message");
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const userWrapper = document.createElement('div')
+    userWrapper.className = 'user-wrapper'
+    const username = document.createElement('p')
+    username.className = 'username'
+    const message = document.createElement('p');
+    message.className = 'message';
+    message.textContent = messageInput.value;
+    userWrapper.appendChild(username)
+    userWrapper.appendChild(message)
+    messagesContainer.appendChild(userWrapper);
+    messageInput.value = ''
+    // add class based on index
+    const allMessages = document.querySelectorAll('.message');
+    const messagesArray = Array.from(allMessages);
+    messagesArray.forEach((message, index) => {
+      if (index % 2 === 0) {
+        username.textContent = "Adeeb"
+        userWrapper.classList.add('wrapper-right')
+        userWrapper.classList.remove('wrapper-left')
+        message.classList.add('message-right');
+        message.classList.remove('message-left');
+      } else {
+        username.textContent = "Joseph"
+        userWrapper.classList.add('wrapper-left')
+        userWrapper.classList.remove('wrapper-right')
+        message.classList.add('message-left');
+        message.classList.remove('message-right');
+      }
+    });
+  });
+  
 
-messageText[0].textContent = "Hi there!";
-messageText[1].textContent = "What's up?";
-messageText[2].textContent = "How have you been?";
-messageText[3].textContent = "Not much. How about you?";
-
-//  clear chat
-
-let count = 0
-
-function clear() {
-    messages.innerHTML = "";
-    count = 0;
-}
-document.getElementById("clear-button").addEventListener("click", clear);
+//  clear
+const clearButton = document
+  .getElementById('clear-button')
+  .addEventListener('click', () => {
+    while (messagesContainer.firstChild) {
+      messagesContainer.removeChild(messagesContainer.firstChild);
+    }
+  });
 
 //  theme
-
 const theme = document.querySelector('#theme-drop-down');
-const lightTheme = document.querySelector('.light-theme');
-const darkTheme = document.querySelector('.dark-theme');
+document.getElementById('main').style.backgroundColor = '#1877F2';
+document.getElementById('messages-container').style.backgroundColor = '#8B4513';
+document.getElementById('messages-container').style.color = '#fff';
 
-darkTheme.onclick = function () {
-    darkTheme.classList.add('.rightRed')
-}
-
-
+theme.addEventListener('click', () => {
+  if (theme.value === 'theme-one') {
+    document.getElementById('main').style.backgroundColor = '#1877F2';
+    document.getElementById('messages-container').style.backgroundColor =
+      '#8B4513';
+  } else if (theme.value === 'theme-two') {
+    document.getElementById('main').style.backgroundColor = 'red';
+    document.getElementById('messages-container').style.backgroundColor =
+      'black';
+  }
+});
